def03c7bbabd1806c9047e35003d20efeacde5b6
Rybik <endyse70@gmail.com>
2023-12-30 03:28:40 +0300

    replace all files back. Try new vercel.json

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const { request } = require("express");
 const express = require("express");
-const categories = require("../routes/categories");
-const sale = require("../routes/sale");
-const order = require("../routes/order");
-const products = require("../routes/products");
-const sequelize = require("../database/database");
+const categories = require("./routes/categories");
+const sale = require("./routes/sale");
+const order = require("./routes/order");
+const products = require("./routes/products");
+const sequelize = require("./database/database");
 const cors = require("cors");
-const Category = require("../database/models/category");
-const Product = require("../database/models/product");
-const PORT = 3333;
+const Category = require("./database/models/category");
+const Product = require("./database/models/product");
+const PORT = process.env.PORT || 3333;
 
 Category.hasMany(Product);
 
